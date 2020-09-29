9a39291845293277d47faa2d135fdc41d1d4d67b
hamida <71189869+Hamidaafrose@users.noreply.github.com>
2020-09-29 15:30:59 +0600

    Add files via upload

---
+++ file_image_upload.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Form</title> 
+</head>
+<body>
+<form id="frm-image-upload" action="upload.php" name='img' method="post" enctype="multipart/form-data">
+    <label for="image">Image</label>
+    <input type="file" class="file-input" name="image"><br><br>
+    <input type="submit" value="submit">
+
+</form>
+</body>
+</html>
